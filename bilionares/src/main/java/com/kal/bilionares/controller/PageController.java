e31045fd35d1abe867ae1c00321b15693021075d
munash48 <mugwanyan@gmail.com>
2019-08-04 16:17:29 +0300

    horizontal nav bar

---
+++ bilionares/src/main/java/com/kal/bilionares/controller/PageController.java
@@ -0,0 +1,20 @@
+package com.kal.bilionares.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value={"/","/home","/index"})
+	public ModelAndView index(){
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Hello Welcome to the billionares");
+		return mv;
+		
+			
+		
+	}
+}
